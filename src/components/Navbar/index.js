4a28924d97659db5597da6ff01414235cbb66744
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2023-04-24 00:29:27 -0400

    theme and responsive nav

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import { Typography, Box, Button, Link, Hidden, Drawer, List, ListItem, useTheme, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+
+const NavContactButton = ({ children, ...props }) => {
+    const theme = useTheme();
+
+    return (
+        <Button
+            variant="contained"
+            sx={{
+                backgroundColor: theme.palette.primary.main,
+                // "&:hover": {
+                //     anim here
+                // },
+                borderRadius: "0",
+                margin: "0 0 0 2.5vw",
+                padding: "1rem",
+                textTransform: "none",
+            }}
+            {...props}
+        >
+            {children}
+        </Button>
+    );
+};
+
+const NavLink = ({ children, ...props }) => {
+    const theme = useTheme();
+
+    return (
+        <Link
+            variant="h6"
+            component="a"
+            sx={{
+                color: theme.palette.primary.main,
+                textDecoration: "none",
+                margin: "0 2.5vw",
+                "&:hover": {
+                    cursor: "pointer",
+                    opacity: 0.7,
+                },
+            }}
+            {...props}
+        >
+            {children}
+        </Link>
+    );
+};
+
+const DrawerOptions = ({ navItems, setDrawerOpen }) => {
+    return (
+        <Box>
+            <List sx={{ display: "flex", flexDirection: "column", margin: "0 2rem 0 0" }}>
+                {navItems.map(({ label, location }) => (
+                    <ListItem key={label} sx={{ marginBottom: "1rem" }}>
+                        <NavLink variant="h6" href={location} onClick={() => setDrawerOpen(false)}>
+                            {label}
+                        </NavLink>
+                    </ListItem>
+                ))}
+                <ListItem sx={{ margin: 0 }}>
+                    <NavContactButton href="#contact" onClick={() => setDrawerOpen(false)}>
+                        <Typography variant="h6">Reach out</Typography>
+                    </NavContactButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+};
+const Navbar = () => {
+    const theme = useTheme();
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const NameLogoSX = {
+        color: theme.palette.primary.main,
+        textDecoration: "none",
+        "&:hover": {
+            cursor: "pointer",
+            opacity: 0.7,
+        },
+    };
+
+    const navItems = [
+        {
+            label: "Home",
+            location: "#home",
+        },
+        {
+            label: "About",
+            location: "#about",
+        },
+        {
+            label: "Experience",
+            location: "#experience",
+        },
+        {
+            label: "Projects",
+            location: "#projects",
+        },
+    ];
+
+    const handleDrawerToggle = () => {
+        setDrawerOpen(!drawerOpen);
+    };
+
+    return (
+        <Box component="nav" sx={{ position: "fixed", top: "0", width: "100%", zIndex: 100 }}>
+            <Box sx={{ display: "flex", justifyContent: "space-between", margin: "2rem 0 2rem 3rem" }}>
+                <Typography variant="h4" component="a" href="#home" sx={{ ...NameLogoSX }}>
+                    Vince
+                    <br /> Moschella
+                </Typography>
+                <Hidden mdDown>
+                    <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                        {navItems.map(({ label, location }) => {
+                            return (
+                                <NavLink key={label} variant="h6" href={location}>
+                                    {label}
+                                </NavLink>
+                            );
+                        })}
+                        <NavContactButton href="#contact">
+                            <Typography variant="h6">Reach out</Typography>
+                        </NavContactButton>
+                    </Box>
+                </Hidden>
+                <Hidden mdUp>
+                    <IconButton
+                        edge="start"
+                        color="#inherit"
+                        aria-label="menu"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: "1rem" }}
+                    >
+                        <MenuIcon sx={{ fontSize: "35px", color: theme.palette.primary.main }} />
+                    </IconButton>
+                    <Drawer
+                        anchor={"right"}
+                        open={drawerOpen}
+                        onClose={handleDrawerToggle}
+                        ModalProps={{ keepMounted: true }}
+                    >
+                        <IconButton
+                            onClick={handleDrawerToggle}
+                            aria-label="close"
+                            sx={{ margin: "2rem 1rem 0rem auto" }}
+                        >
+                            <CloseIcon sx={{ fontSize: "35px", color: theme.palette.primary.main }} />
+                        </IconButton>
+                        <DrawerOptions navItems={navItems} setDrawerOpen={setDrawerOpen} />
+                    </Drawer>
+                </Hidden>
+            </Box>
+        </Box>
+    );
+};
+
+export default Navbar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,51 @@
-import '@/styles/globals.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import "@/styles/globals.css";
+import { Navbar } from "@/components";
+
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 950,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
+    palette: {
+        primary: {
+            main: "#171717",
+        },
+        secondary: {
+            main: "#fff",
+        },
+    },
+    typography: {
+        fontFamily: "MonumentExtended, sans-serif",
+        button: {
+            textTransform: "none",
+        },
+        h1: {
+            fontWeight: 800,
+        },
+        h4: {
+            fontSize: 22,
+        },
+        h6: {
+            fontSize: 16,
+        },
+    },
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Navbar />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
