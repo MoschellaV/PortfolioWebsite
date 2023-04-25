cd69b0294d28939940a1fe488ba91f27619ca150
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2023-04-24 22:06:59 -0400

    hero zoom spline

--- src/components/3dModels/heroModel/index.js
+++ src/components/3dModels/heroModel/index.js
@@ -1,18 +1,33 @@
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 
-const HeroModel = () => {
+const HeroModel = ({ shouldScrollOff }) => {
     return (
         <>
-            <Box sx={{ height: "100vh" }}>
-                <script
-                    type="module"
-                    src="https://unpkg.com/@splinetool/viewer@0.9.304/build/spline-viewer.js"
-                ></script>
-                <spline-viewer
-                    loading-anim
-                    url="https://prod.spline.design/nVr30ytQDIRKxKW6/scene.splinecode"
-                ></spline-viewer>
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "100%",
+                    position: shouldScrollOff ? "static" : "fixed",
+                    visibility: shouldScrollOff ? "hidden" : "visible",
+                    top: "0",
+                    left: "0",
+                    right: "0",
+                    bottom: "0",
+                }}
+            >
+                <Box sx={{ width: "100%", height: "100%" }}>
+                    <script
+                        type="module"
+                        src="https://unpkg.com/@splinetool/viewer@0.9.304/build/spline-viewer.js"
+                    ></script>
+                    <spline-viewer
+                        loading-anim
+                        url="https://prod.spline.design/nVr30ytQDIRKxKW6/scene.splinecode"
+                    ></spline-viewer>
+                </Box>
             </Box>
+            <Box sx={{ minHeight: shouldScrollOff ? "1000px" : "1770px" }}></Box>
         </>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "@/styles/Hero.module.css";
 import { HeroModel } from "@/components";
+import { Box } from "@mui/material";
 
 export default function Home() {
+    const [shouldScrollOff, setShouldScrollOff] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            window.scrollY >= 1000 ? setShouldScrollOff(true) : setShouldScrollOff(false);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <>
             <Head>
@@ -12,8 +28,10 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <HeroModel />
-                <h1>vince</h1>
+                <HeroModel shouldScrollOff={shouldScrollOff} />
+                <Box id="about">
+                    <h1>about</h1>
+                </Box>
             </main>
         </>
     );
