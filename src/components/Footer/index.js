9bd22f6991ca373419a6391d6ce4c9f579a351bb
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2024-08-09 00:43:38 -0400

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 
 const Footer = () => {
+    const currYear = new Date().getFullYear();
+
     return (
         <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", mt: 12, mb: 3 }}>
             <Typography component="p" variant="p">
-                © Vince Moschella
+                {currYear} © Vince Moschella
             </Typography>
         </Box>
     );
