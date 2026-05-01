a0555fb33d6e03c3768e9ad049aff93381a0c55b
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2026-04-30 20:00:51 -0400

    update aws details

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -5,172 +5,191 @@ import SpacingWrapper from "./SpacingWrapper";
 import WorkCard2 from "./WorkCard2";
 
 const ExperienceSection = () => {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const jobInfo = [
-        {
-            company: "Amazon Web Services",
-            companyName: "aws.amazon.com/rds",
-            companyWebsite: "http://aws.amazon.com/rds/oracle/",
-            imageUrl: "/images/companyImages/AWS.png",
-            altText: "AWS Logo",
-            positionTitle: (
-                <>
-                    <span style={{ color: theme.palette.orange.bright }}>Software Development Engineer Intern</span>
-                </>
-            ),
-
-            technologies: "Java, React, Typescript, AWS",
-            descriptionText: "Incoming SDE Intern on RDS for Oracle team building cloud database services",
-            jobDuration: "Sept 2025 - Dec 2025",
-            location: "Vancouver, BC, Canada",
-            isFlipped: false,
-        },
-        {
-            company: "Shopify",
-            companyName: "shopify.com",
-            companyWebsite: "https://www.shopify.com/",
-            imageUrl: "/images/companyImages/Shopify.png",
-            altText: "Shopify Logo",
-            positionTitle: (
-                <>
-                    <span style={{ color: theme.palette.orange.bright }}>Software Engineer Intern</span>
-                </>
-            ),
-
-            technologies: "Typescript, React, Ruby, GraphQL",
-            descriptionText:
-                "I worked on features that made it easier for merchants to set up social logins like Google, Facebook, and Shop for their storefronts. I also focused on strengthening customer account security by building session revocation logic and creating OAuth endpoints that gave merchants more flexibility in how customers sign in.",
-            jobDuration: "May 2025 - Aug 2025",
-            location: "Toronto, ON, Canada",
-            isFlipped: true,
-        },
-        {
-            company: "Ansys",
-            companyName: "ansys.com/fluent",
-            companyWebsite: "https://www.ansys.com/products/fluids/ansys-fluent",
-            imageUrl: "/images/companyImages/Ansys.png",
-            altText: "Ansys Logo",
-            positionTitle: (
-                <>
-                    <span style={{ color: theme.palette.orange.bright }}>Software Engineer Intern</span>
-                </>
-            ),
-
-            technologies: "React, Redux, Cypress",
-            descriptionText:
-                "I helped migrate user preference and workflow features from the Ansys Fluent desktop app to a web interface using React and REST APIs, making high performance simulations accessible through the web. I also wrote end-to-end tests using Cypress to simulate user workflows and improving bug detection and test coverage.",
-            jobDuration: "May 2024 - Dec 2024",
-            location: "Waterloo, ON, Canada",
-            isFlipped: false,
-        },
-        {
-            company: "Google Developer Student Clubs",
-            companyName: "gdscguelph.com",
-            companyWebsite: "https://www.gdscguelph.com/",
-            imageUrl: "/images/companyImages/GDSCLogo.png",
-            altText: "Google Logo",
-            positionTitle: (
-                <>
-                    <span style={{ color: theme.palette.orange.bright }}>President & Software Developer</span>
-                </>
-            ),
-
-            technologies: "Leadership, Team Management, Event Planning, Web/Mobile Development",
-            descriptionText:
-                "I was the lead for the Google Developer Student Club at the University of Guelph during the academic 2023/2024 year! I directed multiple teams, planned workshops, ran a hackathon, and it was all great but what I loved most was being able to build a community of over 250 developers and students. It's awesome seeing people learn and make memories!",
-            jobDuration: "Aug 2023 - Aug 2024",
-            location: "Guelph, ON, Canada",
-            isFlipped: true,
-        },
-        {
-            company: "Saige",
-            companyName: "realsaige.com",
-            companyWebsite: "https://www.realsaige.com/",
-            imageUrl: "/images/companyImages/SaigeLogo.png",
-            altText: "Saige Logo",
-            positionTitle: (
-                <>
-                    <span style={{ color: theme.palette.orange.bright }}>Software Engineer Intern</span>
-                </>
-            ),
-            technologies: "React, Next.js, Python, Flask, SQL, AWS, Firebase, Twilio",
-            descriptionText:
-                "I learned so much during my time at Saige! I managed a team of interns, led the redesign of the company's flagship app, and developed and deployed a multi-channel notification system to production. I really enjoyed working within the start-up's small team as it allowed me to witness firsthand how my contributions had a meaningful impact. I saw how our collaborative efforts translate into tangible improvements for the company and its users.",
-            jobDuration: "Jan 2023 - Aug 2023",
-            location: "Vancouver, BC, Canada",
-            isFlipped: false,
-        },
-        {
-            company: "University of Ottawa",
-            companyName: "uottawa.ca",
-            companyWebsite: "https://www.uottawa.ca/en",
-            imageUrl: "/images/companyImages/UOttawaUniversityLogo.png",
-            altText: "University of Ottawa Logo",
-            positionTitle: (
-                <>
-                    <span style={{ color: theme.palette.orange.bright }}> Undergraduate Research Analyst</span> &mdash;
-                    Clinical COVID-19 Study
-                </>
-            ),
-            technologies: "Python, R, SPSS, Excel",
-            descriptionText:
-                "I had the exciting opportunity to collaborate with researchers and provide statistical analysis insights for a clinical COVID-19 study. I got to dive headfirst into the world of data engineering and gained valuable experience preparing datasets and conducting statistical tests with empirical data.",
-            jobDuration: "June 2023 - July 2023",
-            location: "Toronto, ON, Canada",
-            isFlipped: true,
-        },
-    ];
-
-    return (
+  const jobInfo = [
+    {
+      company: "Amazon Web Services",
+      companyName: "aws.amazon.com/rds",
+      companyWebsite:
+        "https://docs.aws.amazon.com/odb/latest/UserGuide/what-is-odb.html",
+      imageUrl: "/images/companyImages/AWS.png",
+      altText: "AWS Logo",
+      positionTitle: (
         <>
-            <Box
-                id="experience"
-                sx={{
-                    position: "relative",
-                    width: "100vw",
-                    pt: 1,
-                    pb: 1,
-                    backgroundColor: theme.palette.primary.main,
-                }}
-            >
-                <Typography
-                    component="p"
-                    variant="h3"
-                    sx={{
-                        ml: -20,
-                        mr: -500,
-                        WebkitTextStrokeColor: theme.palette.secondary.main,
-                        opacity: 0.1,
-                    }}
-                >
-                    Experience Experience Experience Experience Experience Experience <br />
-                    Experience Experience Experience Experience Experience Experience <br />
-                    Experience Experience Experience Experience Experience Experience <br />
-                    Experience Experience Experience Experience Experience Experience
-                </Typography>
-                <Typography
-                    component="h3"
-                    variant="h2"
-                    sx={{
-                        position: "absolute",
-                        top: "50%",
-                        left: { xs: "51%", sm: "50%" },
-                        transform: "translate(-50%, -50%)",
-                        WebkitTextStrokeWidth: { xs: "1.5px", md: "3px" },
-                        fontSize: { xs: "clamp(1rem, 9vw, 10rem)", sm: 65, md: 100 },
-                    }}
-                >
-                    Experience
-                </Typography>
-            </Box>
-            <Box sx={{ mt: 2 }}>
-                {/* handles rendering each job */}
-                <SpacingWrapper>
-                    <WorkCard2 jobInfo={jobInfo} />
-                </SpacingWrapper>
-            </Box>
-            {/* <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "20vw", m: 5 }}>
+          <span style={{ color: theme.palette.orange.bright }}>
+            Software Development Engineer Intern
+          </span>
+        </>
+      ),
+
+      technologies: "Java, React, Typescript, Lambda, DynamoDB, CloudFormation",
+      descriptionText:
+        "I worked on the Oracle DB on AWS team, where I designed and implemented a Java middleware layer that intercepts incoming requests and delivers customer availability zone mappings to OCI. I also built a custom caching layer which preloaded data from DynamoDB on Lambda initialization so certain lookup opearations stay quick and stable. I also updated storage models and workflow APIs to support private-network integrations for Oracle Database networks shared through AWS Resource Access Manager.",
+      jobDuration: "Sept 2025 - Dec 2025",
+      location: "Vancouver, BC, Canada",
+      isFlipped: false,
+    },
+    {
+      company: "Shopify",
+      companyName: "shopify.com",
+      companyWebsite: "https://www.shopify.com/",
+      imageUrl: "/images/companyImages/Shopify.png",
+      altText: "Shopify Logo",
+      positionTitle: (
+        <>
+          <span style={{ color: theme.palette.orange.bright }}>
+            Software Engineer Intern
+          </span>
+        </>
+      ),
+
+      technologies: "Typescript, React, Ruby, GraphQL",
+      descriptionText:
+        "I worked on features that made it easier for merchants to set up social logins like Google, Facebook, and Shop for their storefronts. I also focused on strengthening customer account security by building session revocation logic and creating OAuth endpoints that gave merchants more flexibility in how customers sign in.",
+      jobDuration: "May 2025 - Aug 2025",
+      location: "Toronto, ON, Canada",
+      isFlipped: true,
+    },
+    {
+      company: "Ansys",
+      companyName: "ansys.com/fluent",
+      companyWebsite: "https://www.ansys.com/products/fluids/ansys-fluent",
+      imageUrl: "/images/companyImages/Ansys.png",
+      altText: "Ansys Logo",
+      positionTitle: (
+        <>
+          <span style={{ color: theme.palette.orange.bright }}>
+            Software Engineer Intern
+          </span>
+        </>
+      ),
+
+      technologies: "React, Redux, Cypress",
+      descriptionText:
+        "I helped migrate user preference and workflow features from the Ansys Fluent desktop app to a web interface using React and REST APIs, making high performance simulations accessible through the web. I also wrote end-to-end tests using Cypress to simulate user workflows and improving bug detection and test coverage.",
+      jobDuration: "May 2024 - Dec 2024",
+      location: "Waterloo, ON, Canada",
+      isFlipped: false,
+    },
+    {
+      company: "Google Developer Student Clubs",
+      companyName: "gdscguelph.com",
+      companyWebsite: "https://www.gdscguelph.com/",
+      imageUrl: "/images/companyImages/GDSCLogo.png",
+      altText: "Google Logo",
+      positionTitle: (
+        <>
+          <span style={{ color: theme.palette.orange.bright }}>
+            President & Software Developer
+          </span>
+        </>
+      ),
+
+      technologies:
+        "Leadership, Team Management, Event Planning, Web/Mobile Development",
+      descriptionText:
+        "I was the lead for the Google Developer Student Club at the University of Guelph during the academic 2023/2024 year! I directed multiple teams, planned workshops, ran a hackathon, and it was all great but what I loved most was being able to build a community of over 250 developers and students. It's awesome seeing people learn and make memories!",
+      jobDuration: "Aug 2023 - Aug 2024",
+      location: "Guelph, ON, Canada",
+      isFlipped: true,
+    },
+    {
+      company: "Saige",
+      companyName: "realsaige.com",
+      companyWebsite: "https://www.realsaige.com/",
+      imageUrl: "/images/companyImages/SaigeLogo.png",
+      altText: "Saige Logo",
+      positionTitle: (
+        <>
+          <span style={{ color: theme.palette.orange.bright }}>
+            Software Engineer Intern
+          </span>
+        </>
+      ),
+      technologies: "React, Next.js, Python, Flask, SQL, AWS, Firebase, Twilio",
+      descriptionText:
+        "I learned so much during my time at Saige! I managed a team of interns, led the redesign of the company's flagship app, and developed and deployed a multi-channel notification system to production. I really enjoyed working within the start-up's small team as it allowed me to witness firsthand how my contributions had a meaningful impact. I saw how our collaborative efforts translate into tangible improvements for the company and its users.",
+      jobDuration: "Jan 2023 - Aug 2023",
+      location: "Vancouver, BC, Canada",
+      isFlipped: false,
+    },
+    {
+      company: "University of Ottawa",
+      companyName: "uottawa.ca",
+      companyWebsite: "https://www.uottawa.ca/en",
+      imageUrl: "/images/companyImages/UOttawaUniversityLogo.png",
+      altText: "University of Ottawa Logo",
+      positionTitle: (
+        <>
+          <span style={{ color: theme.palette.orange.bright }}>
+            {" "}
+            Undergraduate Research Analyst
+          </span>{" "}
+          &mdash; Clinical COVID-19 Study
+        </>
+      ),
+      technologies: "Python, R, SPSS, Excel",
+      descriptionText:
+        "I had the exciting opportunity to collaborate with researchers and provide statistical analysis insights for a clinical COVID-19 study. I got to dive headfirst into the world of data engineering and gained valuable experience preparing datasets and conducting statistical tests with empirical data.",
+      jobDuration: "June 2023 - July 2023",
+      location: "Toronto, ON, Canada",
+      isFlipped: true,
+    },
+  ];
+
+  return (
+    <>
+      <Box
+        id="experience"
+        sx={{
+          position: "relative",
+          width: "100vw",
+          pt: 1,
+          pb: 1,
+          backgroundColor: theme.palette.primary.main,
+        }}
+      >
+        <Typography
+          component="p"
+          variant="h3"
+          sx={{
+            ml: -20,
+            mr: -500,
+            WebkitTextStrokeColor: theme.palette.secondary.main,
+            opacity: 0.1,
+          }}
+        >
+          Experience Experience Experience Experience Experience Experience{" "}
+          <br />
+          Experience Experience Experience Experience Experience Experience{" "}
+          <br />
+          Experience Experience Experience Experience Experience Experience{" "}
+          <br />
+          Experience Experience Experience Experience Experience Experience
+        </Typography>
+        <Typography
+          component="h3"
+          variant="h2"
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: { xs: "51%", sm: "50%" },
+            transform: "translate(-50%, -50%)",
+            WebkitTextStrokeWidth: { xs: "1.5px", md: "3px" },
+            fontSize: { xs: "clamp(1rem, 9vw, 10rem)", sm: 65, md: 100 },
+          }}
+        >
+          Experience
+        </Typography>
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        {/* handles rendering each job */}
+        <SpacingWrapper>
+          <WorkCard2 jobInfo={jobInfo} />
+        </SpacingWrapper>
+      </Box>
+      {/* <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "20vw", m: 5 }}>
                 <Typography
                     variant="h3"
                     component="p"
@@ -185,8 +204,8 @@ const ExperienceSection = () => {
                     more to come...
                 </Typography>
             </Box> */}
-        </>
-    );
+    </>
+  );
 };
 
 export default ExperienceSection;
